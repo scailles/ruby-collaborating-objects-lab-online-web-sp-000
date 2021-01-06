234e0d847ee347aa28453c441f3ee639bfb916d5
Shaina C <scailles23@gmail.com>
2021-01-06 05:44:06 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,22 +1,4 @@
-require_relative("./spec/fixtures/mp3s")
-class MP3Importer
-  
-  attr_accessor :path
-  
-  
-    def initialize(path)
-        @path = path
-    end
 
-    def files
-        files ||= Dir["#{@path}/*"].collect {|song| song.gsub("#{@path}/", "")}
-    end
-
-    def import
-       files.each {|song| Song.new_by_filename(song)}
-    end
-
-end
   
   
   
