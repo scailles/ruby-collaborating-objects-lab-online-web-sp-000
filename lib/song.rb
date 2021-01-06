f96493a0aa194ec0344c1f283c72b7dda0a78299
Shaina C <scailles23@gmail.com>
2021-01-06 05:26:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,11 +25,8 @@ class Song
   
   
   def self.new_by_filename(filename)
-    artist = filename.split(" - ")[0]
-    song = filename.split(" - ")[1]
-    
-    new_song = self.new(song)
-    new_song.artist = Artist.new(artist)
+  self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
     
   end
   
