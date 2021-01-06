7884607a07c909a6810276a2706f1e9eee2b3441
Shaina C <scailles23@gmail.com>
2021-01-06 05:27:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,8 @@ class Song
   
   
   def artist_name=(name)
-    artist.name = artist
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+        artist.add_song(self)
   end
   
   
